54258a07b6ffc57401429e290b6d4692f487b443
ewriq <ozkabalcibirol@gmail.com>
2026-03-23 14:29:37 +0300

    Expose global GORM DB and update server modules
    Make DB a package-level *gorm.DB and properly initialize it: set the sqlite file path, open GORM, check/return errors, and assign the resulting gorm.DB to the package variable. Remove IDE data_source_mapping.xml. Add module requirements to server/go.mod for needed dependencies (indirect).

--- database/init.go
+++ database/init.go
@@ -6,14 +6,21 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-var db *DB
+var DB *gorm.DB
 
 func InitDB() error {
-	DB, err = gorm.Open(sqlite.Open("jdbc:sqlite:C:/Users/Administrator/GolandProjects/come2/data"),
+	dbFile := "C:/Users/Administrator/GolandProjects/come2/data/come2.db"
+
+	gdb, err := gorm.Open(sqlite.Open(dbFile),
 		&gorm.Config{
 			Logger:                 logger.Default.LogMode(logger.Info),
 			SkipDefaultTransaction: true,
 		},
 	)
-	return err
+	if err != nil {
+		return err
+	}
+
+	DB = gdb
+	return nil
 }
--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,13 @@
 module github.com/ewriq/come2/server
 
 go 1.22.2
+
+require (
+	github.com/ewriq/come2 v0.0.0-20260320213647-5bb08155692b // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/driver/sqlite v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
+)
