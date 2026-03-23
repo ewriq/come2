c1cf8c0bd2c8399515e2cac926027d1eb2a51105
ewriq <ozkabalcibirol@gmail.com>
2026-03-23 15:09:25 +0300

    .

--- database/init.go
+++ database/init.go
@@ -1,40 +1,20 @@
 package database
 
 import (
-	"os"
-	"path/filepath"
-
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-var DB *gorm.DB
+var db *gorm.DB
+var err error
 
 func InitDB() error {
-	rootDir := filepath.FromSlash("C:/Users/Administrator/GolandProjects/come2")
-	dbDir := filepath.Join(rootDir, "data")
-	if err := os.MkdirAll(dbDir, 0o755); err != nil {
-		return err
-	}
-
-	dbFile := filepath.Join(dbDir, "come2.db")
-
-	gdb, err := gorm.Open(sqlite.Open(dbFile),
+	db, err = gorm.Open(sqlite.Open("jdbc:sqlite:C:/Users/Administrator/GolandProjects/come2/data"),
 		&gorm.Config{
 			Logger:                 logger.Default.LogMode(logger.Info),
 			SkipDefaultTransaction: true,
 		},
 	)
-	if err != nil {
-		return err
-	}
-
-	DB = gdb
-	return nil
-}
-
-// GetDB returns the initialized *gorm.DB (may be nil if InitDB not called or failed).
-func GetDB() *gorm.DB {
-	return DB
+	return err
 }
